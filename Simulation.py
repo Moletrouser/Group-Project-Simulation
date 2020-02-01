b31cb3a88c058151dbd2ad7007036d5b0f3f2592
Peter Brown <55991907+Moletrouser@users.noreply.github.com>
2020-02-01 18:47:32 +0000

    added collision logic. added box reflections for the nano particle. need to tune the masses and velocities of both particles since the collisions lead to the gas particle leaving box

--- Simulation.py
+++ Simulation.py
@@ -1,6 +1,7 @@
 import vpython as vp
 from vpython import *
 import numpy as np
+import random
 from sympy import *
 
 canvas(width=2400, height=1400)  # slightly bigger than default, adjust if you have small screen.
@@ -8,11 +9,11 @@ canvas(width=2400, height=1400)  # slightly bigger than default, adjust if you h
 dt = 0.0001  # timestep
 maxParticles = 20 ## not implemented yet, to be used in prospective loop to create N gas particles
 
-box = box(pos=vector(0,0,0),length=60, height=60, width=60, opacity=0.2) ## sets the size of the box
+box = box(pos=vector(0,0,0),length=120, height=120, width=120, opacity=0.2) ## sets the size of the box
 
 ## values for the nano particle mass, M, and the gas mass, gasM. Not to scale, PLACEHOLDER VALUES
-M = 1
-gasM = 0.001
+M = 1e-18
+gasM = 5.46e-26
 
 ## marks the centre where potential is zero for reference
 centrePos = vector(0,0,0)
@@ -25,9 +26,9 @@ nanozPos = np.random.normal(10,5,1)
 initNanoPos = vector(nanoxPos, nanoyPos, nanozPos)
 
 ## takes randoms values from a normal dist. for the nano particle velocity (mean 5, standard dev. 0.5) PLACEHOLDER VALUES
-nanoxVel = np.random.normal(3,1,1)
-nanoyVel = np.random.normal(3,1,1)
-nanozVel = np.random.normal(3,1,1)
+nanoxVel = np.random.normal(1,0.2,1)
+nanoyVel = np.random.normal(1,0.2,1)
+nanozVel = np.random.normal(1,0.2,1)
 nanoVel = vector(nanoxVel, nanoyVel, nanozVel)
 
 ## creates the nano particle with values defined above
@@ -36,28 +37,28 @@ nanoParticle.trail_color = color.white  # change the trail colour to white, or a
 nanoVector = nanoParticle.pos
 nanoVectorMag = mag(nanoVector)
 
-## picks a random integer value for each co-ordinate of the gas particles starting location within the box
-gasxPos = np.random.randint(-box.length/2, box.length/2)
-gasyPos = np.random.normal(-box.width/2, box.width/2)
-gaszPos = np.random.normal(-box.height/2,box.height/2)
+## picks a random value for each co-ordinate of the gas particles starting location within the box
+gasxPos = (-box.length/2)+np.random.random()*box.length
+gasyPos = (-box.width/2)+np.random.random()*box.width
+gaszPos = (-box.height/2)+np.random.random()*box.height
 initGasPos = vector(gasxPos, gasyPos, gaszPos)
 
 ## takes randoms values from a normal dist. for the gas velocity (mean 5, standard dev. 0.5) PLACEHOLDER VALUES
-gasxVel = np.random.normal(250,25,1)
-gasyVel = np.random.normal(250,25,1)
-gaszVel = np.random.normal(250,25,1)
+gasxVel = np.random.normal(1000,25,1)
+gasyVel = np.random.normal(1000,25,1)
+gaszVel = np.random.normal(1000,25,1)
 gasVel = vector(gasxVel, gasyVel, gaszVel)
 
 ## creates the gas particle with values defined above
 gasParticle = sphere(pos=initGasPos, radius=0.5, color=color.yellow)
 
 ## creates a set of orthogonal arrows and labels to mark the co-ordinate axis
-xArrow = arrow(pos=vector(40,0,0), axis=vector(15,0,0), shaftwidth=1, color=color.blue)
-xLabel= label( pos=vec(60,0,0), text='x', color=color.blue)
-yArrow = arrow(pos=vector(40,0,0), axis=vector(0,15,0), shaftwidth=1, color=color.green)
-yLabel= label( pos=vec(40,20,0), text='y', color=color.green)
-zArrow = arrow(pos=vector(40,0,0), axis=vector(0,0,15), shaftwidth=1, color=color.red)
-zLabel= label( pos=vec(40,0,20), text='z', color=color.red)
+xArrow = arrow(pos=vector(box.length/2+10,0,0), axis=vector(15,0,0), shaftwidth=1, color=color.blue)
+xLabel= label( pos=vec(box.length/2+20,0,0), text='x', color=color.blue)
+yArrow = arrow(pos=vector(box.width/2+10,0,0), axis=vector(0,15,0), shaftwidth=1, color=color.green)
+yLabel= label( pos=vec(box.width/2+20,20,0), text='y', color=color.green)
+zArrow = arrow(pos=vector(box.height/2+10,0,0), axis=vector(0,0,15), shaftwidth=1, color=color.red)
+zLabel= label( pos=vec(box.height/2+20,0,20), text='z', color=color.red)
 
 ## the entire simulation takes place within this while loop
 while True:
@@ -99,6 +100,8 @@ while True:
     else:
         nanoVel.z = nanoVel.z + dt * restForceMagZ / M
 
+
+
     nanoVector = nanoVector + nanoVel * dt  # defines the new position vector of the nano particle
     nanoParticle.pos = nanoVector  # sets the new nanoParticle position to the updated postion vecotr
     nanoVectorMag = mag(nanoVector)
@@ -107,19 +110,76 @@ while True:
     gasParticle.pos = gasVector # sets the new gas Particle position to the updated position vector
 
     ## checks if the gas particle has collided with the walls of the container, reverses the velocity if so
-    if abs(gasVector.x) + gasParticle.radius >= box.length/2:
-        gasVel.x = -gasVel.x
-    if abs(gasVector.y) + gasParticle.radius >= box.width/2:
-        gasVel.y = -gasVel.y
-    if abs(gasVector.z) + gasParticle.radius >= box.height/2:
-        gasVel.z = -gasVel.z
+    if gasVector.x - gasParticle.radius >= box.length/2:
+        if gasVel.x > 0:
+            gasVel.x = -gasVel.x
+    if gasVector.x + gasParticle.radius <= -box.length/2:
+        if gasVel.x < 0:
+            gasVel.x = -gasVel.x
 
-    ## box.size/2
-    ## +(gasParticle.size/2)
+    if gasVector.y - gasParticle.radius >= box.width/2:
+        if gasVel.y > 0:
+            gasVel.y = -gasVel.y
+    if gasVector.y + gasParticle.radius <= -box.length / 2:
+        if gasVel.y < 0:
+            gasVel.y = -gasVel.y
+
+    if gasVector.z - gasParticle.radius >= box.height/2:
+        if gasVel.z > 0:
+            gasVel.z = -gasVel.z
+    if gasVector.z + gasParticle.radius <= -box.length / 2:
+        if gasVel.z < 0:
+            gasVel.z = -gasVel.z
+
+    ## checks if the nano particle has collided with the walls of the container, reverses the velocity if so
+    if nanoVector.x - nanoParticle.radius >= box.length / 2:
+        if nanoVel.x > 0:
+            nanoVel.x = -nanoVel.x
+    if nanoVector.x + nanoParticle.radius <= -box.length / 2:
+        if nanoVel.x < 0:
+            nanoVel.x = -nanoVel.x
+
+    if nanoVector.y - nanoParticle.radius >= box.width / 2:
+        if nanoVel.y > 0:
+            nanoVel.y = -nanoVel.y
+    if nanoVector.y + nanoParticle.radius <= -box.length / 2:
+        if nanoVel.y < 0:
+            nanoVel.y = -nanoVel.y
+
+    if nanoVector.z - nanoParticle.radius >= box.height / 2:
+        if nanoVel.z > 0:
+            nanoVel.z = -nanoVel.z
+    if nanoVector.z + nanoParticle.radius <= -box.length / 2:
+        if gasVel.z < 0:
+            nanoVel.z = -nanoVel.z
+
+
 
     ## condition to be met if collision has occurred
-    ## needs to be finished
-    #if nanoToGasDistance<(nanoParticle.radius + gasParticle.radius):
+
+    ## if nanoToGasDistance <= (nanoParticle.radius + gasParticle.radius):
+    if nanoToGasDistance <= 7:
+
+        vecX = nanoParticle.pos - gasParticle.pos
+        norm(vecX)
+
+        vecV1 = nanoVel
+        x1 = vecX.dot(vecV1)
+        vecV1x = vecX * x1
+        vecV1y = vecV1 - vecV1x
+        m1 = M
+
+        vecX = vecX*(-1)
+        vecV2 = gasVel
+        x2 = vecX.dot(vecV2)
+        vecV2x = vecX * x2
+        vecV2y = vecV2 - vecV2x
+        m2 = gasM
+
+        nanoVel = vecV1x * (m1 - m2)/(m1 + m2) + vecV2x * (2 * m2)/(m1 + m2) + vecV1y
+        gasVel = vecV1x * (2 * m1) / (m1 + m2) + vecV2x * (m2 - m1) / (m1 + m2) + vecV2y
+
+
 
     rate(1200) ## sets the animation rate
 
